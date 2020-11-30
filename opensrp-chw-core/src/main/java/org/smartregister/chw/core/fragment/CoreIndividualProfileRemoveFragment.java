032591e7f16e3d210a429c3c78a882f7d1f6d29c
Rodgers Andati <andatirodgers@gmail.com>
2020-11-30 16:03:16 +0300

    translate save in RemoveMember fragments

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/fragment/CoreIndividualProfileRemoveFragment.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/fragment/CoreIndividualProfileRemoveFragment.java
@@ -163,7 +163,7 @@ public abstract class CoreIndividualProfileRemoveFragment extends BaseFamilyProf
         Form form = new Form();
         form.setActionBarBackground(org.smartregister.family.R.color.family_actionbar);
         form.setWizard(false);
-        form.setSaveLabel("Save");
+        form.setSaveLabel(this.getResources().getString(R.string.save));
         intent.putExtra(JsonFormConstants.JSON_FORM_KEY.FORM, form);
 
         startActivityForResult(intent, JsonFormUtils.REQUEST_CODE_GET_JSON);
